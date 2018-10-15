2cc3ffa147b39e87b92e8f4e0b176aaff6dff12f
lxd <lixuande@neotel.com.cn>
2018-10-15 13:46:29 +0800

    update code format

--- files/rt2860v2/os/linux/rt_netlink.c
+++ files/rt2860v2/os/linux/rt_netlink.c
@@ -1,29 +1,17 @@
-/*
- ***************************************************************************
- * Ralink Tech Inc.
- * 4F, No. 2 Technology 5th Rd.
- * Science-based Industrial Park
- * Hsin-chu, Taiwan, R.O.C.
+/*--------------------------------------------------------
+ *  Copyright(C) 2018 EASTCOM-BUPT Inc.
  *
- * (c) Copyright 2002, Ralink Technology, Inc.
+ *  Author      : even li
+ *  Description : even li at ebupt.com
+ *  History     : 2018-10-15 Created
  *
- * All rights reserved. Ralink's source code is an unpublished work and the
- * use of a copyright notice does not imply otherwise. This source code
- * contains confidential trade secret material of Ralink Tech. Any attemp
- * or participation in deciphering, decoding, reverse engineering or in any
- * way altering the source code is stricitly prohibited, unless the prior
- * written consent of Ralink Technology, Inc. is obtained.
- ***************************************************************************
-
-    Module Name:
-    rt_netlink.c
-
-    Abstract:
-    Create and register netlink system for ralink device
-
-    Revision History:
-    Who         When            What
-    --------    ----------      ----------------------------------------------
+ *   Module Name:
+ *   rt_netlink.c
+ *
+ *   Abstract:
+ *   Create and register netlink system for ralink device
+ *
+ *--------------------------------------------------------
 */
 
 #include <linux/kernel.h>
@@ -72,8 +60,7 @@ int send_status_to_user(void);
 
 int send_onestatus_to_user(unsigned char *mac, bool bestop);
 
-int send_onestatus_to_user(unsigned char *mac, bool bestop) //
-{
+int send_onestatus_to_user(unsigned char *mac, bool bestop) {
     int size;
     struct sk_buff *skb;
     unsigned char *old_tail;
@@ -83,12 +70,9 @@ int send_onestatus_to_user(unsigned char *mac, bool bestop) //
     
     unsigned char info[16];
     memset(info, 0, 16);
-    if (bestop)
-    {
+    if (bestop){
     	info[0] = 9;
-    }
-    else
-    {
+    }else{
     	info[0] = 4;
     	info[1] = 4;
     	memcpy(&info[2], mac, 6);
@@ -102,14 +86,14 @@ int send_onestatus_to_user(unsigned char *mac, bool bestop) //
     info[13] = 0;
     info[14] = 0;
 
-    size = NLMSG_SPACE(strlen(info)); //
-    skb = alloc_skb(size, GFP_ATOMIC); //,GFP_ATOMIC>
+    size = NLMSG_SPACE(strlen(info)); 
+    skb = alloc_skb(size, GFP_ATOMIC); 
 
     nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(strlen(info))-sizeof(struct nlmsghdr), 0); 
     old_tail = skb->tail;
 	
     memcpy(NLMSG_DATA(nlh), info, strlen(info));
-    nlh->nlmsg_len = skb->tail - old_tail; //
+    nlh->nlmsg_len = skb->tail - old_tail; 
 
     NETLINK_CB(skb).dst_group = 1;
 
@@ -117,8 +101,8 @@ int send_onestatus_to_user(unsigned char *mac, bool bestop) //
     
     return 0;
 }
-int send_status_to_user(void)
-{
+
+int send_status_to_user(void){
 	struct  net_device		*net_dev;
 	RTMP_ADAPTER		    *pAd;
 	unsigned char           info[16];
@@ -128,27 +112,22 @@ int send_status_to_user(void)
 
 	int size;
 	int index=0;
-  struct sk_buff *skb;
-  unsigned char *old_tail;
-  struct nlmsghdr *nlh; 
+    struct sk_buff *skb;
+    unsigned char *old_tail;
+    struct nlmsghdr *nlh; 
 
 	#if 1
 	extern struct net_device* getadapter(VOID);
 	net_dev = getadapter();
-	if (net_dev != NULL)
-	{
+	if (net_dev != NULL){
 		GET_PAD_FROM_NET_DEV(pAd, net_dev);
-	}
-	else
-	{
+	}else{
 		return -1;
 	}
 	
-	for(index=0; index<HASH_TABLE_SIZE; index++)
-	{
+	for(index=0; index<HASH_TABLE_SIZE; index++){
 		pEntry = pAd->MacTab.Hash[index];
-		while (pEntry && !IS_ENTRY_NONE(pEntry))
-		{
+		while (pEntry && !IS_ENTRY_NONE(pEntry)){
 		    send_onestatus_to_user(pEntry->Addr, false);
 				pEntry = pEntry->pNext;
 				
@@ -181,12 +160,11 @@ date:
 	10/08/2015
 
 ********************************************************************/
-int send_detectdata_to_user(unsigned char *mac, wifi_detect detect)
-{
+int send_detectdata_to_user(unsigned char *mac, wifi_detect detect){
 	int size;
     struct sk_buff *skb;
     unsigned char *old_tail;
-    struct nlmsghdr *nlh; //
+    struct nlmsghdr *nlh; 
 
     int retval;
 
@@ -204,34 +182,22 @@ int send_detectdata_to_user(unsigned char *mac, wifi_detect detect)
     info[12] = detect.snr1;
     info[13] = detect.snr2;
 
-    size = NLMSG_SPACE(strlen(info)); //
-    skb = alloc_skb(size, GFP_ATOMIC); //,GFP_ATOMIC>
-
-    //netlink
+    size = NLMSG_SPACE(strlen(info)); 
+    skb = alloc_skb(size, GFP_ATOMIC); 
     nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(strlen(info))-sizeof(struct nlmsghdr), 0); 
     old_tail = skb->tail;
-    //memcpy(NLMSG_DATA(nlh), info, strlen(info)); //
+    
     memcpy(NLMSG_DATA(nlh), info, strlen(info));
-    nlh->nlmsg_len = skb->tail - old_tail; //
+    nlh->nlmsg_len = skb->tail - old_tail; 
 
-    //
-    //NETLINK_CB(skb).pid = 0;
     NETLINK_CB(skb).dst_group = 1;
 
-		//DBGPRINT(RT_DEBUG_ERROR, ("------SKB--------%02x:%02x:%02x:%02x:%02x:%02x------------\n", skb->data[2],skb->data[3],skb->data[4],skb->data[5],skb->data[6],skb->data[7]));
-		//DBGPRINT(RT_DEBUG_ERROR, ("------SKB--------%04x:%04x:%04x:%04x:%04x:%04x------------\n", ((unsigned char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[2],((char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[3],((char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[4],((char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[5],((char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[6],((char *)NLMSG_DATA((struct nlmsghdr *)skb->data))[7]));
-    //printk(KERN_ERROR "[kernel space] skb->data:%s\n", (char *)NLMSG_DATA((struct nlmsghdr *)skb->data));
-
-    //
-    //DBGPRINT(RT_DEBUG_ERROR, ("------netlink--------%02x:%02x:%02x:%02x:%02x:%02x------------\n", info[2],info[3],info[4],info[5],info[6],info[7]));
-    //retval = netlink_unicast(netlinkfd, skb, user_process.pid, MSG_DONTWAIT);
     retval = netlink_broadcast(netlinkfd, skb, 0, 1, GFP_ATOMIC);
     
     return 0;
 }
 
-void kernel_receive(struct sk_buff *__skb) 
-{
+void kernel_receive(struct sk_buff *__skb) {
     struct sk_buff *skb;
     struct nlmsghdr *nlh = NULL;
 
@@ -244,11 +210,8 @@ void kernel_receive(struct sk_buff *__skb)
         if((nlh->nlmsg_len >= sizeof(struct nlmsghdr))
             && (__skb->len >= nlh->nlmsg_len)){
             user_process.pid = nlh->nlmsg_pid;
-            //printk(KERN_ERR "[kernel space]1 data receive from user are:%s\n", (char *)NLMSG_DATA(nlh));
-            //send_to_user(data);
-            if (strcmp((char *)NLMSG_DATA(nlh), "g") == 0)
-            {
-							send_status_to_user();
+            if (strcmp((char *)NLMSG_DATA(nlh), "g") == 0){
+                send_status_to_user();
             }
         }
     }else{
@@ -259,11 +222,10 @@ void kernel_receive(struct sk_buff *__skb)
     kfree_skb(skb);
 }
 
-int rt_netlink_init(void)
-{
-		struct netlink_kernel_cfg cfg = {
-    .input = kernel_receive,
-		};
+int rt_netlink_init(void){
+	struct netlink_kernel_cfg cfg = {
+        .input = kernel_receive,
+	};
 		
     netlinkfd = netlink_kernel_create(&init_net, NETLINK_TEST,  &cfg);
     if(!netlinkfd){
@@ -272,8 +234,8 @@ int rt_netlink_init(void)
     }
     return 0;
 }
+
 int rt_netlink_exit(void)
-{
     sock_release(netlinkfd->sk_socket);
     printk(KERN_DEBUG "test_netlink_exit!!\n");
 }
