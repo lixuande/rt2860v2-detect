eac53d9f08d26f49226df83df28d6689d1c27ac8
even <evenlixd@gmail.com>
2026-05-09 10:52:38 +0800

    Update rt_netlink.c

--- files/rt2860v2/os/linux/rt_netlink.c
+++ files/rt2860v2/os/linux/rt_netlink.c
@@ -1,8 +1,7 @@
 /*--------------------------------------------------------
- *  Copyright(C) 2018 EASTCOM-BUPT Inc.
  *
  *  Author      : even li
- *  Description : even li at ebupt.com
+ *  Description : even li 
  *  History     : 2018-10-15 Created
  *
  *   Module Name:
